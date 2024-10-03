faf8097dc9a13d35bab911fc85eeddb10e0324b0
chris6896 <159868610+chris6896@users.noreply.github.com>
2024-10-03 17:18:05 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CalculatorApp());
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator App',
+      home: CalculatorHome(),
+    );
+  }
+}
+
+class CalculatorHome extends StatelessWidget {
+  @override
+  Widget buildButton(String buttonText) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.all(24.0),
+        ),
+        child: Text(
+          buttonText,
+          style: TextStyle(fontSize: 20.0),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculator"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+            child: Text(
+              '0',
+              style: TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,                
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Row(
+                children: [ 
+                  buildButton("7"),
+                  buildButton("8"),
+                  buildButton("9"),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton("4"),
+                  buildButton("5"),
+                  buildButton("6"),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton("1"),
+                  buildButton("2"),
+                  buildButton("3"),
+                ],
+              ),
+              Row(
+                children: [
+                  buildButton("0")
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
